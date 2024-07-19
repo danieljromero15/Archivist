38791069acad560617a58fffd1e1fe5e7fc0f337
JackTFB <jacktfbofficial@gmail.com>
2024-07-19 01:07:08 -0400

    Improved guide with screenshots and basic formatting

--- lib/pages/guide.dart
+++ lib/pages/guide.dart
@@ -2,6 +2,7 @@ import 'package:archivist/nav_bar.dart';
 import 'package:archivist/pages/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:path/path.dart';
 
 class GuidePage extends StatefulWidget {
   const GuidePage({super.key, this.title = 'API Keys Guide'});
@@ -35,40 +36,102 @@ class _GuidePageState extends State<GuidePage> {
       ),
       body: const Align(
         alignment: Alignment.topCenter,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
+        child: SingleChildScrollView(
+          child: SizedBox(
+            width: 1600,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                //TODO Add images and clean up the text to make it more presentable.
+                Text(
+                  style: TextStyle(
+                    fontSize: 24.0
+                  ),
+                    'This is a quick guide to help you set up your API keys to use Archivist.'),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                    'As of right now, we haven\'t found a way to properly implement a user account system, so for now, you\'ll have to add your own API keys.'),
+                Text(
+                    'Fortunately, all you need is a Twitch account with 2FA enabled.'),
+                SizedBox(height: 50,),
+                Text(
+                    style: TextStyle(
+                        fontSize: 24.0
+                    ),
+                    'STEP 1: SET UP YOUR API KEYS'),
+                Text('First, go to dev.twitch.tv and sign into your account.'),
+                SizedBox(height: 25,),
+                Text('Second, click the Go To Console button on the top right.'),
+                SizedBox(height: 25,),
+                Image(
+                  height: 500,
+                  image: AssetImage('assets/Screenshot_1.png'), ),
+                SizedBox(height: 25,),
+                Text('Third, select applications on the left side of the menu'),
+                SizedBox(height: 25,),
+                Image(
+                    height: 500,
+                    image: AssetImage('assets/Screenshot_2.png')),
+                SizedBox(height: 25,),
+                Text('Fourth, select Register your Application'),
+                SizedBox(height: 25,),
+                Image(
+                    height: 500,
+                    image: AssetImage('assets/Screenshot_3.png')),
+                SizedBox(height: 25,),
+                Text(
+                    'Finally, set your settings as follows.  Have the name be any name you want, but it has to be unique.  '
+                        'Set your OAuth field to localhost with a random port.  My recommendation is \'https://localhost:8080\''
+                        'Set your Category to Application Implementation, and set your client type to Confidential.'),
+                SizedBox(height: 25,),
+                Image(
+                    height: 500,
+                    image: AssetImage('assets/Screenshot_4.png')),
+                SizedBox(height: 50,),
+                Text(
+                    style: TextStyle(
+                        fontSize: 24.0
+                    ),
+                    'STEP 2: IMPORT YOUR API KEYS INTO ARCHIVIST'),
+                Text(
+                    'Once you\'ve created your application, you can now grab your API keys.'),
+                SizedBox(height: 25,),
+                Image(
+                    height: 500,
+                    image: AssetImage('assets/Screenshot_8.png')),
+                Text(
+                    'On your newly created application, select Manage, and at the bottom you\'ll need two strings of text for your API keys.'),
+                SizedBox(height: 25,),
+                Image(
+                    height: 500,
+                    image: AssetImage('assets/Screenshot_5.png')),
+                SizedBox(height: 25,),
+                Text(
+                    'First is your Client ID.  This is considered your API key in Archivist.  Copy it and paste it into the API Key field in Settings.'),
+                SizedBox(height: 25,),
+                Image(image: AssetImage('assets/Screenshot_6.png')),
+                SizedBox(height: 25,),
+                Text(
+                    'Next is your Client Secret.  This is considered your Secret Key in Archvist.  Click the New Secret button, and copy-paste the newly added text below the button into the Secret Key field in Settings.'),
+                SizedBox(height: 25,),
+                Image(
+                    height: 500,
+                    image: AssetImage('assets/Screenshot_7.png')),
+                SizedBox(height: 25,),
+                Text(
+                    'Once you\'ve finished both tasks, you are done!  Make sure to test if your keys work by using the Test API button in the settings page.  If the text \'Test Passed\' shows up, you are ready to use Archivist!'),
+                SizedBox(height: 25,),
+                Image(
+                    height: 500,
+                    image: AssetImage('assets/Screenshot_9.png')),
+                SizedBox(height: 50,),
+              ],
             ),
-            //TODO Add images and clean up the text to make it more presentable.
-            Text(
-                'This is a quick guide to help you set up your API keys to use Archivist.'),
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-                'As of right now, we haven\'t found a way to properly implement a user account system, so for now, you\'ll have to add your own API keys.'),
-            Text(
-                'Fortunately, all you need is a Twitch account with 2FA enabled.'),
-            Text('STEP 1: SET UP YOUR API KEYS'),
-            Text('First, go to dev.twitch.tv and sign into your account.'),
-            Text('Second, click the Go To Console button on the top right.'),
-            Text('Third, select applications on the left side of the menu'),
-            Text('Fourth, select Register your Application'),
-            Text(
-                'Finally, set your settings as follows.  Have the name be any name you want, but it has to be unique.  Set your OAuth field to localhost with a random port.  My recommendation is \'https://localhost:8080\''),
-            Text('STEP 2: IMPORT YOUR API KEYS INTO ARCHIVIST'),
-            Text(
-                'Once you\'ve created your application, you can now grab your API keys.'),
-            Text(
-                'On your newly created application, select Manage, and at the bottom you\'ll need two strings of text for your API keys.'),
-            Text(
-                'First is your Client ID.  This is considered your API key in Archivist.  Copy it and paste it into the API Key field in Settings.'),
-            Text(
-                'Next is your Client Secret.  This is considered your Secret Key in Archvist.  Click the New Secret button, and copy-paste the newly added text below the button into the Secret Key field in Settings.'),
-            Text(
-                'Once you\'ve finished both tasks, you are done!  You can now use Archivist freely!  Eventually we\'ll get around to adding Twitch integration, but at a later date XD'),
-          ],
+          ),
         ),
       ),
     );
